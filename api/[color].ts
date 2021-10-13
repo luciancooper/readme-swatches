7daf155fd59fba0516a8d79cd6bc8833220b2cd1
Lucian Cooper <cooperlucian@gmail.com>
2021-10-13 18:21:08 -0400

    feat: add padding query parameters

--- api/[color].ts
+++ api/[color].ts
@@ -16,11 +16,17 @@ const svgAttr = "xmlns='http://www.w3.org/2000/svg' xmlns:xlink='http://www.w3.o
 export default function endpoint(req: VercelRequest, res: VercelResponse) {
     const color = parseColor(queryValue(req.query['color']!)) ?? '#000000',
         size = Number(queryValue(req.query['size'] ?? '20')),
+        top = Number(queryValue(req.query['top'] ?? req.query['y'] ?? '0')),
+        bottom = Number(queryValue(req.query['bottom'] ?? req.query['y'] ?? '0')),
+        left = Number(queryValue(req.query['left'] ?? req.query['x'] ?? '0')),
+        right = Number(queryValue(req.query['right'] ?? req.query['x'] ?? '0')),
         radius = Number(queryValue(req.query['r'] ?? req.query['radius'] ?? '0')),
         shape = radius >= size / 2
-            ? `<circle fill='${color}' cx='${size / 2}' cy='${size / 2}' r='${size / 2}'/>`
-            : `<rect fill='${color}' x='0' y='0' width='${size}' height='${size}' rx='${radius}'/>`,
-        svg = `<svg ${svgAttr} width='${size}' height='${size}' viewBox='0 0 ${size} ${size}'>${shape}</svg>`;
+            ? `<circle fill='${color}' cx='${left + size / 2}' cy='${top + size / 2}' r='${size / 2}'/>`
+            : `<rect fill='${color}' x='${left}' y='${top}' width='${size}' height='${size}' rx='${radius}'/>`,
+        width = left + size + right,
+        height = top + size + bottom,
+        svg = `<svg ${svgAttr} width='${width}' height='${height}' viewBox='0 0 ${width} ${height}'>${shape}</svg>`;
     // set response header
     res.setHeader('Content-Type', 'image/svg+xml');
     // send response
